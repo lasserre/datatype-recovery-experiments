08301565d07c0df8e805df94887f9bcae3498eb5
lasserre <cstew33@gmail.com>
2023-09-27 09:59:34 -0500

    Created a jupyter notebook to start analyzing dataset

---
+++ datatype_recovery/experiments/dataset_analysis.py
@@ -0,0 +1,16 @@
+
+def is_notebook() -> bool:
+    '''Returns True if this is executing in a Jupyter notebook'''
+    try:
+        shell = get_ipython().__class__.__name__
+        if shell == 'ZMQInteractiveShell':
+            return True   # Jupyter notebook or qtconsole
+        elif shell == 'TerminalInteractiveShell':
+            return False  # Terminal running IPython
+        else:
+            return False  # Other type (?)
+    except NameError:
+        return False      # Probably standard Python interpreter
+
+def characterize_dataset():
+    pass
